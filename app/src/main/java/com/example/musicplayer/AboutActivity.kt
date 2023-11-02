e932710bce99b632a33e3dbe7e7a7ed9fae3efb6
jaydip <pjaydip956@gmail.com>
2023-11-02 21:00:54 +0530

    Commit

--- app/src/main/java/com/example/musicplayer/AboutActivity.kt
+++ app/src/main/java/com/example/musicplayer/AboutActivity.kt
@@ -15,7 +15,12 @@ class AboutActivity : AppCompatActivity() {
         aboutText.text = aboutText()
     }
     private fun aboutText(): String{
-        return "Developed By: Divy M Patel" +
-                "\n\nIf you want to provide feedback, I will love to hear that."
+        return "About\n" +
+                "\n" +
+                "Welcome to Music Nova, the ultimate destination for all your music needs! We offer a vast selection of songs from all genres and eras, so you're sure to find something you love. Plus, with our personalized recommendations and easy-to-use interface, you'll be able to discover new music and create custom playlists in no time.\n" +
+                "\n" +
+                "Whether you're looking to listen to your favorite hits or discover new artists, Music Nova has you covered. We also offer exclusive features like offline listening, high-quality audio, and ad-free listening, so you can enjoy your music without interruption.\n" +
+                "\n" +
+                "So what are you waiting for? Download Music Nova today and start listening to your favorite songs!"
     }
 }
